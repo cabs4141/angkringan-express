83e7f4c4a0dd78f464de547f9986866567ef1a8a
binda rama <crakun95@gmail.com>
2024-08-25 12:37:16 +0800

    cors puqi

--- index.js
+++ index.js
@@ -20,14 +20,7 @@ import cors from "cors";
 //express
 const app = express();
 
-const corsOptions = {
-  origin: "http://localhost:5173", // Alamat frontend Anda
-  methods: ["GET", "POST", "PUT", "DELETE"], // Metode HTTP yang diizinkan
-  allowedHeaders: ["Content-Type", "Authorization"], // Header yang diizinkan
-  credentials: true, // Jika Anda menggunakan kredensial (misalnya cookie)
-};
-
-app.use(cors(corsOptions));
+app.use(cors());
 
 //dotenv
 dotenv.config();
